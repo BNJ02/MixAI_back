7eaf734a2b7c3b97398ec34471c00e52d8104203
Benjamin Lepourtois <benji372002@gmail.com>
2024-10-11 13:15:02 +0200

    Inscription BDD + test Authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { User } from './user/user.entity';
+import { UsersModule } from './user/users.module';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,11 +15,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: 'azerty',
       database: 'mixai',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
